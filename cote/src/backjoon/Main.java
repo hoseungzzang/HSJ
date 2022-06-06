d9ed8acf0a05a5e7a10569e40b9376477ce0d05f
hs <hs@192.168.219.107>
2022-06-06 15:08:52 +0900

    백준알고리즘 기초부수기

---
+++ cote/src/backjoon/Main.java
@@ -0,0 +1,155 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int roop = Integer.parseInt(br.readLine());
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		ArrayList<Integer> list2 = new ArrayList<Integer>();
+		int[] arr = new int[roop];
+		int[] count = new int[roop];
+		int[] choiarr = {};
+		int sum =0;
+		for(int i=0; i<roop; i++) {
+			arr[i]  = Integer.parseInt(br.readLine());
+			sum+=arr[i];
+			if(list.indexOf(arr[i])==-1) {
+				list.add(arr[i]);
+				count[i]++;
+			}else {
+				count[list.indexOf(arr[i])]++;
+			}
+		}
+		int choi= Arrays.stream(count).max().getAsInt();
+		
+		double calc = (double)sum/roop;//산술평균
+		String s= String.format("%.0f",calc);
+		if(s.equals("-0")) {
+			s="0";
+		}
+		bw.write(s+"\n");
+		if(roop>1) {
+			for(int i=0; i<roop; i++) {
+				if(count[i]==choi) {
+					list2.add(arr[i]);
+				}
+			}
+			if(list2.size()>1) {
+				choiarr = new int[list2.size()];
+				for(int i=0; i<choiarr.length;i++) {
+					choiarr[i] = list2.get(i);
+				}
+				Arrays.sort(choiarr);
+				choi = choiarr[1];
+			}
+			else choi = list2.get(0);
+			
+			Arrays.sort(arr);
+			int middle = arr[roop/2];
+			int beom = arr[roop-1]-arr[0];
+			bw.write(middle+"\n"+choi+"\n"+beom);
+		}else {
+			bw.write(arr[0]+"\n"+arr[0]+"\n"+"0");
+		}
+		bw.flush();
+		
+	}
+
+	public static void ex1() throws IOException {
+		// 블랙잭 모든경우탐색
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer str = new StringTokenizer(br.readLine());
+		int num = Integer.parseInt(str.nextToken());
+		int max = Integer.parseInt(str.nextToken());
+		str = new StringTokenizer(br.readLine());
+		int[] arr = new int[num];
+		for (int i = 0; i < num; i++) {
+			arr[i] = Integer.parseInt(str.nextToken());
+
+		}
+		int sum = 0;
+		int answer = 0;
+		int calc = max;
+		for (int i = 0; i < num; i++) {
+			for (int j = i + 1; j < num; j++) {
+				if (j == num - 1) {
+					break;
+				}
+				for (int k = j + 1; k < num; k++) {
+					sum = arr[i] + arr[j] + arr[k];
+					if (max - sum >= 0 && max - sum < calc) {
+						calc = max - sum;
+						answer = sum;
+					}
+				}
+			}
+		}
+		System.out.println(answer);
+	}
+
+	public static void ex2() throws IOException {
+		// 분해합
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer str = new StringTokenizer(br.readLine());
+
+		String num = str.nextToken();
+		int min = num.length() * 9;
+		int max = Integer.parseInt(num);
+		int sum = 0;
+		int index = max - min;
+		if (max <= 8) {
+			index = 0;
+		}
+		for (int i = index; i < max; i++) {
+			sum = 0;
+			String arr = Integer.toString(i);
+			for (int j = 0; j < arr.length(); j++) {
+				sum += arr.charAt(j) - '0';
+			}
+			sum += i;
+			if (sum == max) {
+				bw.write(i + "");
+				sum = 1;
+				break;
+			}
+		}
+		if (sum != 1) {
+			bw.write(0 + "");
+		}
+		bw.flush();
+
+	}
+
+	public static void ex3() throws IOException {
+		// 정렬 2nlogn
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int roop = Integer.parseInt(br.readLine());
+		int[] arr = new int[roop];
+		for (int i = 0; i < roop; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < roop; i++) {
+			bw.write(arr[i] + "\n");
+		}
+		bw.flush();
+
+	}
+
+}
--- cote/src/cote/study.java
+++ cote/src/cote/study.java
@@ -19,30 +19,7 @@ import java.io.OutputStreamWriter;
 public class study {
 
 	public static void main(String[] args) throws IOException {
-		
-		int[] array = {1, 5, 2, 6, 3, 7, 4};
-		
-		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-		 int[] answer = new int[commands.length];
-		
-		for(int i=0; i<commands.length;i++) {
-			ArrayList<Integer> list = new ArrayList<Integer>();
-			int num1 = commands[i][0]-1;
-			int num2 = commands[i][1]-1;
-			if(num1!=num2) {
-				for(int j=num1; j<=num2; j++){
-					list.add(array[j]);
-				}
-				int [] arr = new int[list.size()];
-				for(int j=0; j<list.size();j++) {
-					arr[j] = list.get(j);
-				}
-				Arrays.sort(arr);
-				answer[i] = arr[commands[i][2]-1];
-			}
-			else answer[i] = array[num1];
-		}
-		
+
 		
 	}
 
@@ -91,282 +68,319 @@ public class study {
 		} else
 			answer += s.charAt(num);
 	}
+
 	public static void ex3() throws IOException {
-		//쉬운문재
-		int price =3;
+		// 쉬운문재
+		int price = 3;
 		int money = 20;
 		int count = 4;
-		long answer= -1;
+		long answer = -1;
 		int calc = 0;
-		for(int i=1; i<=count; i++) {
-			calc+=price*i;
+		for (int i = 1; i <= count; i++) {
+			calc += price * i;
 		}
-		if(calc>money) {
-			System.out.println(calc-money);
-		}
-		else System.out.println(0);
-		
+		if (calc > money) {
+			System.out.println(calc - money);
+		} else
+			System.out.println(0);
+
 	}
+
 	public static void ex4() throws IOException {
-		//정렬문제/
-		int[] numbers = {2,1,3,4,1};
+		// 정렬문제/
+		int[] numbers = { 2, 1, 3, 4, 1 };
 		int num = 0;
 		int[] answer = {};
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		Arrays.sort(numbers);
-		for(int i=0; i<numbers.length;i++) {
-			for(int j=i+1; j<numbers.length;j++) {
-				num = numbers[i]+numbers[j];
-				if(list.indexOf(num)==-1) {
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				num = numbers[i] + numbers[j];
+				if (list.indexOf(num) == -1) {
 					list.add(num);
 				}
 			}
 		}
-		answer =new int[list.size()];
-		
-		for(int i=0; i<list.size();i++) {
-		answer[i]=list.get(i);	
+		answer = new int[list.size()];
+
+		for (int i = 0; i < list.size(); i++) {
+			answer[i] = list.get(i);
 		}
-		
-		
+
 	}
+
 	public static void ex5() throws IOException {
-		//쉬운문제
-		int[] arr= {5, 9, 7, 10};
-		
+		// 쉬운문제
+		int[] arr = { 5, 9, 7, 10 };
+
 		int divisor = 5;
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		int[] answer = {};
-		for(int i=0; i<arr.length;i++) {
-			if(arr[i]%divisor==0) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] % divisor == 0) {
 				list.add(arr[i]);
 			}
 		}
-		if(list.size()==0) {
-			
-		}else
-		{
-			for(int i=0; i<list.size();i++) {
+		if (list.size() == 0) {
+
+		} else {
+			for (int i = 0; i < list.size(); i++) {
 				answer[i] = list.get(i);
 			}
 			Arrays.sort(answer);
 		}
-		
+
 	}
+
 	public static void ex6() throws IOException {
-		//캘린더 활용문제
+		// 캘린더 활용문제
 		Calendar cal = Calendar.getInstance();
 		cal.set(2016, 4, 24);
 		int da = cal.get(Calendar.DAY_OF_WEEK);
-		String day="";
-		switch(da) {
+		String day = "";
+		switch (da) {
 		case 1:
-			day ="SUN";
+			day = "SUN";
 			break;
 		case 2:
-			day ="MON";
+			day = "MON";
 			break;
 		case 3:
-			day ="TUE";
+			day = "TUE";
 			break;
 		case 4:
-			day ="WEN";
+			day = "WEN";
 			break;
 		case 5:
-			day ="THU";
+			day = "THU";
 			break;
 		case 6:
-			day ="FRI";
+			day = "FRI";
 			break;
 		case 7:
-			day ="SAT";
+			day = "SAT";
 			break;
 		}
 		System.out.println(day);
 	}
+
 	public static void ex7() throws IOException {
-		//진법계산 및 sb 리버스 사용
-		int n=125;
-		String nd="";
-		while(n>0) {
-			nd = (n%3) +nd;
-			n/=3;
+		// 진법계산 및 sb 리버스 사용
+		int n = 125;
+		String nd = "";
+		while (n > 0) {
+			nd = (n % 3) + nd;
+			n /= 3;
 		}
 		StringBuffer sb = new StringBuffer(nd);
 		String answer1 = sb.reverse().toString();
-		int answer = Integer.parseInt(answer1,3);
+		int answer = Integer.parseInt(answer1, 3);
 		System.out.println(answer);
-	
-		
+
 	}
+
 	public static void ex8() throws IOException {
-		//예산계산 쉬웠음.
-		int[] d = {2,2,3,3};
-		int budget =10;
-		int cnt=0;
+		// 예산계산 쉬웠음.
+		int[] d = { 2, 2, 3, 3 };
+		int budget = 10;
+		int cnt = 0;
 		Arrays.sort(d);
-		for(int i=0; i<d.length;i++) {
-			if(d[i]>budget) {
+		for (int i = 0; i < d.length; i++) {
+			if (d[i] > budget) {
 				break;
 			}
-			budget-=d[i];
+			budget -= d[i];
 			cnt++;
 		}
 		System.out.println(cnt);
-		
-	}
-	
-	public static void ex9() throws IOException {
-		//포켓몬찾기
 
-		
-		int[] nums = {3,1,2,3};
-		int cnt = nums.length/2;
+	}
+
+	public static void ex9() throws IOException {
+		// 포켓몬찾기
+
+		int[] nums = { 3, 1, 2, 3 };
+		int cnt = nums.length / 2;
 		int answer = 0;
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
-		for(int i=0; i<nums.length;i++) {
-			if(arrayList.indexOf(nums[i])==-1) {
+		for (int i = 0; i < nums.length; i++) {
+			if (arrayList.indexOf(nums[i]) == -1) {
 				arrayList.add(nums[i]);
 			}
 		}
-		if(arrayList.size()>=cnt) {
+		if (arrayList.size() >= cnt) {
 			answer = cnt;
-		}
-		else answer = arrayList.size();
-		
-	
-		
+		} else
+			answer = arrayList.size();
+
 	}
+
 	public static void ex10() throws IOException {
-		//비밀지도
-		int n=5;
-		int[] arr1= {9, 20, 28, 18, 11};
-		int[] arr2 = {30, 1, 21, 17, 28};
+		// 비밀지도
+		int n = 5;
+		int[] arr1 = { 9, 20, 28, 18, 11 };
+		int[] arr2 = { 30, 1, 21, 17, 28 };
 		String[] answer = new String[n];
-		for(int i=0; i<arr1.length;i++) {
-			String a=Integer.toBinaryString(arr1[i]);
-			String b=Integer.toBinaryString(arr2[i]);
-			if(a.length()<n) {
-				int cnt = n-a.length();
-				for(int j=0; j<cnt;j++) {
-					a = "0"+a;
+		for (int i = 0; i < arr1.length; i++) {
+			String a = Integer.toBinaryString(arr1[i]);
+			String b = Integer.toBinaryString(arr2[i]);
+			if (a.length() < n) {
+				int cnt = n - a.length();
+				for (int j = 0; j < cnt; j++) {
+					a = "0" + a;
 				}
 			}
-			
-			if(b.length()<n) {
-				int cnt = n-b.length();
-				for(int j=0; j<cnt;j++) {
-					b = "0"+b;
+
+			if (b.length() < n) {
+				int cnt = n - b.length();
+				for (int j = 0; j < cnt; j++) {
+					b = "0" + b;
 				}
 			}
-			
-			answer[i]="";
-		
-			for(int j=0; j<n;j++) {
-				int sum = (a.charAt(j)-'0') +(b.charAt(j)-'0');
-				if(sum>0) {
-					answer[i]+="#";
-				}
-				else answer[i]+=" ";
+
+			answer[i] = "";
+
+			for (int j = 0; j < n; j++) {
+				int sum = (a.charAt(j) - '0') + (b.charAt(j) - '0');
+				if (sum > 0) {
+					answer[i] += "#";
+				} else
+					answer[i] += " ";
 			}
 		}
 
-		
 	}
-	public static void ex11() throws IOException {
-		//소수찾아홀짝덧뺼셈
 
-		
+	public static void ex11() throws IOException {
+		// 소수찾아홀짝덧뺼셈
+
 		int left = 13;
 		int right = 17;
 		int answer = 0;
-		for(int i=left; i<=right;i++) {
-			int cnt=0;
-			for(int j=1; j<=i; j++) {
-				if(i%j==0) {
+		for (int i = left; i <= right; i++) {
+			int cnt = 0;
+			for (int j = 1; j <= i; j++) {
+				if (i % j == 0) {
 					cnt++;
 				}
 			}
-			
-			if(cnt%2==0) {
+
+			if (cnt % 2 == 0) {
 				answer += i;
-			}
-			else answer-= i;
+			} else
+				answer -= i;
 		}
 		System.out.println(answer);
-		
-	
 
-		
 	}
-	
+
 	public static void ex12() throws IOException {
-		//확률검증
-		int N=5;
-		int[] stages= {2, 1, 2, 6, 2, 4, 3, 3};
+		// 확률검증
+		int N = 5;
+		int[] stages = { 2, 1, 2, 6, 2, 4, 3, 3 };
 		int stageCnt = stages.length;
-		double [] arr = new double[N];
-		double [] arr2 = new double[N];
-		double [] arr3 = new double[N];
-		int sum=stageCnt;
+		double[] arr = new double[N];
+		double[] arr2 = new double[N];
+		double[] arr3 = new double[N];
+		int sum = stageCnt;
 		int[] answer = new int[N];
-		for(int i=1; i<=N; i++){
-			int cnt=0;
-			for(int j=0; j<stageCnt; j++) {
-				if(i==stages[j]) {
+		for (int i = 1; i <= N; i++) {
+			int cnt = 0;
+			for (int j = 0; j < stageCnt; j++) {
+				if (i == stages[j]) {
 					cnt++;
 				}
 			}
-			if(cnt==0) {
-				arr[i-1]=0.0;
-			}
-			else arr[i-1] = (double)cnt/sum*100;
-			
-			arr2[i-1] = arr[i-1];
-			sum -=cnt;
+			if (cnt == 0) {
+				arr[i - 1] = 0.0;
+			} else
+				arr[i - 1] = (double) cnt / sum * 100;
+
+			arr2[i - 1] = arr[i - 1];
+			sum -= cnt;
 		}
 		Arrays.sort(arr2);
-		for(int i=arr.length-1;i>=0; i--) {
-			arr3[N-1-i] = arr2[i];	
+		for (int i = arr.length - 1; i >= 0; i--) {
+			arr3[N - 1 - i] = arr2[i];
 		}
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr.length; j++) {
-				if(arr3[i]==arr[j]) {
-					answer[i] = j+1;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (arr3[i] == arr[j]) {
+					answer[i] = j + 1;
 					arr[j] = 0.1234;
 					break;
 				}
 			}
 		}
-		
-		
+
 	}
+
 	public static void ex13() throws IOException {
-		//정렬
-		int[] array = {1, 5, 2, 6, 3, 7, 4};
-		
-		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-		 int[] answer = new int[commands.length];
-		
-		for(int i=0; i<commands.length;i++) {
+		// 정렬
+		int[] array = { 1, 5, 2, 6, 3, 7, 4 };
+
+		int[][] commands = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
+		int[] answer = new int[commands.length];
+
+		for (int i = 0; i < commands.length; i++) {
 			ArrayList<Integer> list = new ArrayList<Integer>();
-			int num1 = commands[i][0]-1;
-			int num2 = commands[i][1]-1;
-			if(num1!=num2) {
-				for(int j=num1; j<=num2; j++){
+			int num1 = commands[i][0] - 1;
+			int num2 = commands[i][1] - 1;
+			if (num1 != num2) {
+				for (int j = num1; j <= num2; j++) {
 					list.add(array[j]);
 				}
-				int [] arr = new int[list.size()];
-				for(int j=0; j<list.size();j++) {
+				int[] arr = new int[list.size()];
+				for (int j = 0; j < list.size(); j++) {
 					arr[j] = list.get(j);
 				}
 				Arrays.sort(arr);
-				answer[i] = arr[commands[i][2]-1];
-			}
-			else answer[i] = array[num1];
+				answer[i] = arr[commands[i][2] - 1];
+			} else
+				answer[i] = array[num1];
 		}
-		
-		
+
 	}
 
+	public static void ex14() throws IOException {
+		// 숫자 중복삭제
+		int[] arr = { 1, 1, 3, 3, 0, 1, 1 };
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(arr[0]);
+		int cnt = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (cnt != arr[i]) {
+				list.add(arr[i]);
+				cnt = arr[i];
+			}
+		}
+		int[] answer = new int[list.size()];
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = list.get(i);
+			System.out.println(answer[i]);
+		}
+
+	}
+
+	public static void ex15() throws IOException {
+		// 65점
+		int n = 3;
+		int[] lost = { 3 };
+		int[] reserve = { 1 };
+		int answer = 0;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int cnt = n - lost.length;
+		for (int i = 0; i < lost.length; i++) {
+			for (int j = 0; j < reserve.length; j++) {
+				if ((lost[i] == (reserve[j] - 1) || lost[i] == (reserve[j] + 1)) && list.indexOf(reserve[j]) == -1) {
+					cnt++;
+					list.add(reserve[j]);
+					break;
+				}
+			}
+		}
+		answer = cnt;
+		System.out.println(answer);
+
+	}
 }
