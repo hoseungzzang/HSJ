631b28e72ec13e068733e0e7a7fd5c9e0f4f1647
hoseungzzang <wnghtmd99@naver.com>
2022-10-15 15:57:22 +0900

    ps

--- cote/src/cote/lv2.java
+++ cote/src/cote/lv2.java
@@ -23,16 +23,33 @@ import java.util.stream.Collectors;
 public class lv2 {
 
 	public static void main(String[] args) throws IOException {
-		int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
-		 int answer = 0;
-		for(int i=1; i<land.length; i++) {
-			land[i][0] = land[i][0] + Math.max(land[i-1][3],Math.max(land[i-1][1],land[i-1][2] )) ;  
-			land[i][1] = land[i][1] +Math.max(land[i-1][3],Math.max(land[i-1][0],land[i-1][2] ));  
-			land[i][2] = land[i][2] + Math.max(land[i-1][3],Math.max(land[i-1][1],land[i-1][0] ));  
-			land[i][3] = land[i][3] + Math.max(land[i-1][1],Math.max(land[i-1][0],land[i-1][2] ));  
-		}
-		answer = Math.max(Math.max(land[land.length-1][0], land[land.length-1][1]), Math.max(land[land.length-1][0], land[land.length-1][1]));
-		System.out.println(answer);
+		String s = "aabbaccc";
+		  int answer = s.length();    // 압축하기 전 문자열 길이로 초기화
+	        
+	        for(int i = 1; i <= s.length() / 2; i++){
+	            int zipLevel = 1;                           // 현재 압축정도
+	            String zipStr = s.substring(0, i);          // 압축할 문자
+	            StringBuilder result = new StringBuilder(); // 압축완료한 문자를 저장할 StringBuilder
+	            
+	            for(int j = i; j <= s.length(); j += i){
+	                // 다음 문자 추출
+	                String next = s.substring(j, j + i > s.length() ? s.length() : i + j);
+	                // 다음 문자와 현재 문자가 같으면 zipLevel증가
+	                if(zipStr.equals(next)) zipLevel++;
+	                // 다음 문자와 현재 문자가 다를 경우
+	                else{
+	                    // (압축이 안됬을 경우는 공백, 압축이 됬을경우 zipLevel을 붙여줌) + 압축할 문자를 넣어줌, 
+	                    result.append((zipLevel != 1 ? zipLevel : "") + zipStr);
+	                    zipStr = next;      // 다음 문자를 압축할 문자로 지정
+	                    zipLevel = 1;       // 압축 정도 1로 초기화
+	                }
+	            }
+	            result.append(zipStr);      // 마지막에 추가안된 zipStr추가
+	            answer = Math.min(answer, result.length()); // 가장 작은 문자열 길이 저장
+	        }
+	        System.out.println(answer);
+	        
+		
 	}
 		
 
@@ -1424,6 +1441,18 @@ public class lv2 {
 				answer = triangle[triangle.length-1][i];
 			}
 		}
+		System.out.println(answer);
+	}
+	public static void ex45() throws IOException {
+		int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+		 int answer = 0;
+		for(int i=1; i<land.length; i++) {
+			land[i][0] = land[i][0] + Math.max(land[i-1][3],Math.max(land[i-1][1],land[i-1][2] )) ;  
+			land[i][1] = land[i][1] +Math.max(land[i-1][3],Math.max(land[i-1][0],land[i-1][2] ));  
+			land[i][2] = land[i][2] + Math.max(land[i-1][3],Math.max(land[i-1][1],land[i-1][0] ));  
+			land[i][3] = land[i][3] + Math.max(land[i-1][1],Math.max(land[i-1][0],land[i-1][2] ));  
+		}
+		answer = Math.max(Math.max(land[land.length-1][0], land[land.length-1][1]), Math.max(land[land.length-1][0], land[land.length-1][1]));
 		System.out.println(answer);
 	}
 	
--- cote/src/cote/test.java
+++ cote/src/cote/test.java
@@ -1,13 +1,114 @@
 package cote;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 import java.util.Stack;
 
 public class test {
+	
+	static boolean [] check;
+	static int answer=0;
+	static int [] by ;
+	static HashMap<Integer,String> map = new HashMap<>();
 	public static void main(String[] args) throws IOException {
+		/*
+		String source = "bbaabd";
+		String dest="" ;
+		String answer ="";
+		for(char i = 'a' ; i<='z'; i++) {
+			if(source.contains(Character.toString(i))) {
+				source = source.replaceFirst(Character.toString(i), "");
+				dest += i;
+			}
+		}
+		String [] arr = source.split("");
+		Arrays.sort(arr);
+		answer = dest.concat(String.join("", arr));		
+		System.out.println(answer);
+		*/
+		test2();
+	}
+	
+	public static void test2() {
+		int [] music = {10,9,4,5,12};
+		int start =1;
+		map.put(1, "white");
+		map.put(3, "white");
+		map.put(5, "white");
+		map.put(7, "white");
+		map.put(8, "white");
+		map.put(10, "white");
+		map.put(12, "white");
+		map.put(2, "black");
+		map.put(4, "black");
+		map.put(6, "black");
+		map.put(9, "black");
+		map.put(11, "black");
 		
-		String a = "0110";
-		System.out.println(Integer.parseInt(a));
-		
+		for(int i=0; i<music.length; i++) {
+			check = new boolean[13];
+			by = new int [13];
+			check[0]= true;
+			BFS(start,music[i]);
+			start = music[i]; 
+		}
+		System.out.println(answer);
+	}
+	public static void BFS(int start, int end) {
+		Queue<Integer> que = new LinkedList<Integer>(); 
+		Queue<Integer> index = new LinkedList<Integer>(); 
+		que.add(start);
+		index.add(0);
+		check[start]= true;
+		while(!que.isEmpty()) {
+			int num = que.poll();
+			int id = index.poll();
+			if(num == end) {
+				answer+=id;
+				break;
+			}
+			if(map.get(num).equals("white")) {
+				for(int i=1; i<=2; i++) {
+					if(num-i>0 && !check[num-i]) {
+						if(num==8 && i==2){
+							
+						}else {
+							que.offer(num-i);
+							index.offer(id+1);
+							check[num-i] = true;
+						}
+						
+					}
+					if(num+i<13 && !check[num+i]) {
+						if(num == 7 && i==2) {
+							
+						}else {
+							que.offer(num+i);
+							index.offer(id+1);
+							check[num+i] = true;
+						}
+						
+					}
+				}
+			}else {
+				if(!check[num+1]) {
+					que.offer(num+1);
+					index.offer(id+1);
+					check[num+1] = true;
+				}
+				if(!check[num-1]) {
+					que.offer(num-1);
+					index.offer(id+1);
+					check[num-1] = true;
+				}
+			}
+		}
 	}
 }
